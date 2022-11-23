bbd4a74ee5e2e4d47021a4b70b5b35679c51cbf0
Alexandre Zamberlan <alexz@ufn.edu.br>
2022-11-23 15:39:31 -0300

    lista versus arvores

---
+++ anotacoesAlexandre/5-Equals_CompareTo_HashCode/Colecoes/Program.cs
@@ -0,0 +1,83 @@
+﻿//List<string> palavras = new List<string>();
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine("Palavra: ");
+//    palavras.Add(Console.ReadLine());
+//}
+
+//Console.WriteLine("Digite algo para pesquisar:");
+//string pesquisa = Console.ReadLine();
+
+//if (!palavras.Contains(pesquisa))
+//{
+//    palavras.Add("nome2");
+//}
+
+//palavras.Sort();
+
+//foreach (var item in palavras)
+//{
+//    Console.WriteLine(item);
+//}
+
+using Colecoes;
+
+//List<Pessoa> pessoas = new List<Pessoa>();
+//string nome;
+//string cpf;
+//DateOnly dataNascimento;
+//Pessoa p;
+
+//for (int i = 0; i < 2; i++)
+//{
+//    Console.Write("Nome: ");
+//    nome = Console.ReadLine();
+
+//    Console.Write("CPF: ");
+//    cpf = Console.ReadLine();
+
+//    Console.Write("Data Nascimento: ");
+//    dataNascimento = DateOnly.Parse(Console.ReadLine());
+
+//    p = new Pessoa(nome, cpf, dataNascimento);
+
+//    if (!pessoas.Contains(p)) //ainda é uma busca sequencial
+//    {
+//        pessoas.Add(p);
+//    }
+//}
+
+
+SortedSet<Pessoa> pessoas = new SortedSet<Pessoa>();
+Random gerador = new Random();
+int numero;
+for (int i = 0; i < 10; i++)
+{
+    numero = gerador.Next(10);
+    if (!pessoas.Add(new Pessoa("Nome" + numero, ""+numero, DateOnly.Parse("12/12/2000"))));
+    {
+        System.Console.WriteLine("Pessoa com matrícula já cadastrado");
+    }
+}
+
+foreach (var i in pessoas)
+{
+    Console.WriteLine(i);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
